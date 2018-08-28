cc8709cc0d50f1eb6de614100bf8d094f8b19dae
skazi019 <skazi019@gmail.com>
2018-08-28 21:04:03 +0530

    Only Embarked column remainig plus improve the model.

---
+++ kaggle_titanic_atom.py
@@ -0,0 +1,70 @@
+# %%
+print('Hello')
+
+#%%
+import numpy as np
+import pandas as pd
+
+# importing the Datasets.
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+# Checking the Datasets
+train.head()
+test.head()
+
+# Checking the shape of Datasets
+train.shape
+test.shape
+
+# Selecting columns that will be used in Predicting.
+train = train[['PassengerId', 'Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
+train.head()
+
+test = test[['PassengerId', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
+test.head()
+
+# Checking the Datasets for missing values.
+train.info()
+
+# Filling the missing values of Age column with its mean.
+train_mean = train[['Age']].mean()
+train[['Age']] = train[['Age']].fillna(train_mean)
+train.info()
+
+# Doing the same with Test Datasets
+test_mean = test[['Age']].mean()
+test[['Age']] = test[['Age']].fillna(test_mean)
+test.info()
+
+# Converting Sex from catergorical to numerical
+train['Sex'] = train['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+train.head()
+
+test['Sex'] = test['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+test.head()
+
+#train['Embarked'].unique()
+
+# Splitting the Train Datasets
+X = train.drop('Survived', axis=1)
+y = train['Survived']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Training the Dataset using RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(max_depth=20, max_leaf_nodes=20, max_features=7, min_samples_split=10)
+rfc.fit(X_train, y_train)
+
+# Checking the Accuracy Score of the Model
+from sklearn.metrics import accuracy_score
+print('Training accuracy: ', accuracy_score(y_train, rfc.predict(X_train)))
+print('Validation accuracy: ', accuracy_score(y_test, rfc.predict(X_test)))
+prediciton = rfc.predict(test)
+test['Survived'] = prediciton
+test.head()
+test = test[['PassengerId', 'Survived']]
+test.head()
+test.to_csv('atom_res.csv', index=False)
